96dcbdd27dd6e6818ee162ee26c431cfddb62d10
Melotto <melotto@gmail.com>
2019-05-21 12:16:27 -0400

    Update Week02.java
    Edit: Inserted comment line as class exercise

--- src/main/java/Week02.java
+++ src/main/java/Week02.java
@@ -1,12 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 /**
  *
  * @author melotto
+ * This line was edited on 21/05, classroom exercise
  */
 import java.util.*;
 public class Week02 {
