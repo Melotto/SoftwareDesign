101ac4e1ec9f7d32474b73d3531a57d30b841908
melotto <melotto@gmail.com>
2019-05-21 13:24:31 -0400

    test

---
+++ SoftDesign/src/SoftDesign/Week02.java
@@ -0,0 +1,28 @@
+package SoftDesign;
+import java.util.*;
+
+public class Week02 {
+
+	Random rand = new Random();
+	
+	public int roll () {
+		int randValue = rand.nextInt(16) +1 ;
+		return randValue;
+	}
+	
+	public static void main(String[] args) {
+		Week02 self = new Week02();
+		int randNo = 0;
+		double sum = 0.0;
+		
+		for(int i=0; i<20; i++){
+			randNo = self.roll();
+			sum += randNo;
+			System.out.println(randNo);
+		}
+		System.out.println("Average rolls is: " + sum/20);
+		
+	}
+
+
+}
